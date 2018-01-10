58804158eabc5935bd7f0f92ab120b4e673e3237
Damian Gover <c3109954@uon.edu.au>
2018-01-10 14:08:04 +1100

    Initialise with Local Copy of Prototype Conversion

---
+++ functions.php
@@ -0,0 +1,31 @@
+<?php
+
+if (!function_exists('ce_setup')) :
+
+    function ce_setup()
+    {
+        add_theme_support('title-tag');
+    }
+
+endif;
+
+add_action('after_setup_theme', 'ce_setup');
+
+function register_custom_menus() {
+    register_nav_menus(
+        array (
+            'primary' => __('Header Position'),
+            'footer' => __('Footer Position')
+        )
+    );
+}
+
+add_action('init', 'register_custom_menus');
+
+function register_stylesheets() {
+    wp_enqueue_style('ce_style', get_stylesheet_uri());
+//    wp_enqueue_style('ce_font', 'https://fonts.googleapis.com/css?family=Loras');
+}
+
+add_action('wp_enqueue_scripts', 'register_stylesheets');
+
