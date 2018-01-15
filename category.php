80251eb10650209ea56f4c7de798cff3841f400e
Damian Gover <c3109954@uon.edu.au>
2018-01-15 17:48:53 +1100

    Markup Changes for Category Page
    - Seperate divs from semantic elements

--- category.php
+++ category.php
@@ -1,40 +1,51 @@
 <?php get_header(); ?>
 
-<main class="content">
-    <section>
-        <h1><?php single_cat_title(); ?></h1>
-        <?php
-            $active_category = get_category( get_query_var( 'cat' ) );
-            $active_category_id = $active_category->cat_ID;
-            echo category_description( $active_category_id );
-        ?>
-    </section>
+    <div class="content">
+        <main>
+            <div class="category-blurb">
+                <section>
+                    <h1><?php single_cat_title(); ?></h1>
+                    <?php
+                    $active_category = get_category( get_query_var( 'cat' ) );
+                    $active_category_id = $active_category->cat_ID;
+                    echo category_description( $active_category_id );
+                    ?>
+                </section>
+            </div>
 
-    <hr />
+            <div class="related-post-container">
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <div class="related-post">
+                        <article>
+                            <header class="entry-header">
+                                <?php
 
-    <div class="related-post-container">
+                                if (is_single()) {
+                                    the_title('<h3 class="entry-title">', '</h1>');
+                                } elseif (is_front_page() && is_home()) {
+                                    the_title('<h3 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h1>');
+                                } else {
+                                    the_title('<h3 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+                                }
 
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                                ?>
+                            </header>
+                            <div class="excerpt"><?php the_excerpt(); ?></div>
+                        </article>
+                    </div>
+                <?php endwhile; endif; ?>
+            </div>
 
-        <section class="related-post">
-            <header class="entry-header">
-                <?php
-
-                if (is_single()) {
-                    the_title('<h3 class="entry-title">', '</h1>');
-                } elseif (is_front_page() && is_home()) {
-                    the_title('<h3 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h1>');
-                } else {
-                    the_title('<h3 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-                }
-
-                ?>
-            </header>
-            <?php the_excerpt(); ?>
-        </section>
-
-        <?php endwhile; endif; ?>
+            <div class="subscribe">
+                <p>Strategies for change. Straight to your inbox.</p>
+                <form>
+                    <fieldset>
+                        <input type="email" required placeholder="Email Address" class="big-text"/>
+                        <input type="submit" value="Join" class="big-button"/>
+                    </fieldset>
+                </form>
+            </div>
+        </main>
     </div>
-</main>
 
 <?php get_footer(); ?>
