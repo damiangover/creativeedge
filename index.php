14bd7f705563ee3aabe24d6262340e914294b0fd
Damian Gover <c3109954@uon.edu.au>
2018-01-16 15:27:02 +1100

    Fade In Main Content
    - By default through index

--- index.php
+++ index.php
@@ -8,46 +8,46 @@
         your browser</a> to improve your experience and security.</p>
     <![endif]-->
 
-    <main class="content">
+    <div class="content animate-text--2s">
+        <main>
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header class="entry-header">
+                        <?php
 
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <header class="entry-header">
-                    <?php
+                        if (is_single()) {
+                            the_title('<h1 class="entry-title">', '</h1>');
+                        } elseif (is_front_page() && is_home()) {
+                            the_title('<h1 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h1>');
+                        } else {
+                            the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+                        }
 
-                    if (is_single()) {
-                        the_title('<h1 class="entry-title">', '</h1>');
-                    } elseif (is_front_page() && is_home()) {
-                        the_title('<h1 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h1>');
-                    } else {
-                        the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-                    }
+                        ?>
+                    </header>
+                    <section>
+                        <?php
+                        /* translators: %s: Name of current post */
+                        the_content(sprintf(
+                            __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'creativeedge'),
+                            get_the_title()
+                        ));
 
-                    ?>
-                </header>
-                <section>
-                    <?php
-                    /* translators: %s: Name of current post */
-                    the_content(sprintf(
-                        __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'creativeedge'),
-                        get_the_title()
-                    ));
+                        wp_link_pages(array(
+                            'before' => '<div class="page-links">' . __('Pages:', 'creativeedge'),
+                            'after' => '</div>',
+                            'link_before' => '<span class="page-number">',
+                            'link_after' => '</span>',
+                        ));
+                        ?>
+                    </section>
+                </article>
 
-                    wp_link_pages(array(
-                        'before' => '<div class="page-links">' . __('Pages:', 'creativeedge'),
-                        'after' => '</div>',
-                        'link_before' => '<span class="page-number">',
-                        'link_after' => '</span>',
-                    ));
-                    ?>
-                </section>
-            </article>
+            <?php endwhile; endif; ?>
 
-        <?php endwhile; endif; ?>
-
-    </main>
-
+        </main>
+    </div>
 <?php
     get_footer();
 ?>
