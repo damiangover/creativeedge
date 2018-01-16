6d5acf50b2fd715c286eb5a169b1fd92f7cf5b89
Damian Gover <c3109954@uon.edu.au>
2018-01-16 15:28:27 +1100

    Remove Subscribe Form from Footer
    - File tidy

--- footer.php
+++ footer.php
@@ -1,15 +1,3 @@
-
-
-<!--<div class="subscribe">-->
-<!--    <p>Strategies for change. Straight to your inbox.</p>-->
-<!--    <form>-->
-<!--        <fieldset>-->
-<!--            <input type="email" required placeholder="Email Address" class="big-text"/>-->
-<!--            <input type="submit" value="Join" class="big-button"/>-->
-<!--        </fieldset>-->
-<!--    </form>-->
-<!--</div>-->
-
 <!--<script src="js/vendor/modernizr-3.5.0.min.js"></script>-->
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"
         integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
@@ -33,6 +21,9 @@
         $("main").addClass("animate-out");
     });
 </script>
+
+<?php wp_footer() ?>
+
 </div>
 </body>
 </html>
