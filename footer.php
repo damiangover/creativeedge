04fd05d126c2d9dfffeb0baed5cc0440e198d854
Damian Gover <c3109954@uon.edu.au>
2018-01-16 15:23:23 +1100

    Fade Out Animations on Unload
    - Fade main content out when unloading a page

--- footer.php
+++ footer.php
@@ -11,9 +11,9 @@
 <!--</div>-->
 
 <!--<script src="js/vendor/modernizr-3.5.0.min.js"></script>-->
-<!--<script src="https://code.jquery.com/jquery-3.2.1.min.js"-->
-<!--        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>-->
-<!--<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>-->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
 <!--<script src="js/plugins.js"></script>-->
 <!--<script src="js/main.js"></script>-->
 
@@ -27,6 +27,12 @@
 <!--    ga('send', 'pageview')-->
 <!--</script>-->
 <!--<script src="https://www.google-analytics.com/analytics.js" async defer></script>-->
+<script>
+    window.addEventListener("beforeunload", function () {
+        console.log("Calling animate out");
+        $("main").addClass("animate-out");
+    });
+</script>
 </div>
 </body>
 </html>
