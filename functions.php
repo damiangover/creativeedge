c608214309128bfe443029ecd07333169fe8bd06
Damian Gover <c3109954@uon.edu.au>
2018-01-18 15:08:08 +1100

    Import Webfonts
    - Playing with pairings
    - Far from final, there is a lot of whitespace with these pairings

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@ add_action('init', 'register_custom_menus');
 
 function register_stylesheets() {
     wp_enqueue_style('ce_style', get_stylesheet_uri());
-//    wp_enqueue_style('ce_font', 'https://fonts.googleapis.com/css?family=Loras');
+    wp_enqueue_style('ce_font', 'https://fonts.googleapis.com/css?family=Montserrate|Crimson+Text|Lato');
 }
 
 add_action('wp_enqueue_scripts', 'register_stylesheets');
