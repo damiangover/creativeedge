70ec144355f6617e7ad5f96ce537ecdf0189f4be
Damian Gover <c3109954@uon.edu.au>
2018-02-20 18:25:23 +1100

    Simplify Front Page

--- page-front.php
+++ page-front.php
@@ -1,54 +1,31 @@
 <?php
-    /* Template Name: Front Page Template */
-    get_header();
+/* Template Name: Front Page Template */
+get_header();
 ?>
 
-    <!--[if lte IE 9]>
-    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a
-            href="https://browsehappy.com/">upgrade
-        your browser</a> to improve your experience and security.</p>
-    <![endif]-->
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <section class="hero">
+        <?php
+        the_title('<h1>', '</h1>');
+        ?>
+        <?php
+        /* translators: %s: Name of current post */
+        the_content(sprintf(
+            __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'creativeedge'),
+            get_the_title()
+        ));
 
-    <div class="content front animate-text--3s">
-        <main>
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        wp_link_pages(array(
+            'before' => '<div class="page-links">' . __('Pages:', 'creativeedge'),
+            'after' => '</div>',
+            'link_before' => '<span class="page-number">',
+            'link_after' => '</span>',
+        ));
+        ?>
+    </section>
 
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <header class="entry-header">
-                        <?php
+<?php endwhile; endif; ?>
 
-                        if (is_single()) {
-                            the_title('<h1 class="entry-title">', '</h1>');
-                        } elseif (is_front_page() && is_home()) {
-                            the_title('<h1 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h1>');
-                        } else {
-                            the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-                        }
-
-                        ?>
-                    </header>
-                    <section>
-                        <?php
-                        /* translators: %s: Name of current post */
-                        the_content(sprintf(
-                            __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'creativeedge'),
-                            get_the_title()
-                        ));
-
-                        wp_link_pages(array(
-                            'before' => '<div class="page-links">' . __('Pages:', 'creativeedge'),
-                            'after' => '</div>',
-                            'link_before' => '<span class="page-number">',
-                            'link_after' => '</span>',
-                        ));
-                        ?>
-                    </section>
-                </article>
-
-            <?php endwhile; endif; ?>
-
-        </main>
-    </div>
 <?php
-    get_footer();
+get_footer();
 ?>
