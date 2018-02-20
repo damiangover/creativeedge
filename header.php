42c86b16eec229be327c1bb20e7f23e600a11481
Damian Gover <c3109954@uon.edu.au>
2018-02-20 16:31:20 +1100

    Adjust Navigation Menu Structure

--- header.php
+++ header.php
@@ -11,36 +11,37 @@
 
 ?>
 
-<!DOCTYPE html>
+    <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="profile" href="http://gmpg.org/xfn/11">
 
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title><?php wp_title(); ?></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title><?php wp_title(); ?></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="manifest" href="site.webmanifest">
-    <link rel="apple-touch-icon" href="icon.png">
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href="icon.png">
 
-    <?php if (is_singular() && pings_open(get_queried_object())) : ?>
-        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-    <?php endif; ?>
+        <?php if (is_singular() && pings_open(get_queried_object())) : ?>
+            <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+        <?php endif; ?>
 
-    <?php wp_head(); ?>
-</head>
+        <?php wp_head(); ?>
+    </head>
 
-<body>
-<div class="grid-container">
-    <div class="logo">Creative Edge</div>
-    <nav>
+    <body>
+    <div class="grid-container">
+        <div class="logo">Creative Edge</div>
         <?php
-        wp_nav_menu($arg = array(
-            'theme_location' => 'primary'
+        wp_nav_menu($args = array(
+            'theme_location' => 'primary',
+            'container' => 'nav',
+            'container_class' => 'horizontal-navigation',
+            'menu_class' => 'flatten',
         ));
         ?>
-    </nav>
