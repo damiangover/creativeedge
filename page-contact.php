b868cc3ffe9081e8723e019b9dd55b7a4a75a633
Damian Gover <c3109954@uon.edu.au>
2018-02-22 10:31:40 +1100

    Add Contact Static Page
    * Re-evaluate later

---
+++ page-contact.php
@@ -0,0 +1,37 @@
+<?php
+/* Template Name: Contact Page Template */
+    get_header();
+?>
+
+    <main class="content animate-text--2s">
+        <div class="contact">
+            <div class="contact-section">
+                <header>About</header>
+                <section>
+                    Dr. Garzitto has been working with energy medicine modalities since 2002. Her PhD research in
+                    conflict sparked an interest in cell memory and the role it plays in health and well being especially around
+                    trauma and chronic injuries. Her passion in combining these areas of knowledge continues to evolve her
+                    practice. She is passionate about sharing her experience, knowledge and expertise with her clients and
+                    continues to work at enhancing her knowledge base.
+                </section>
+            </div>
+            <div class="contact-section">
+                <header>Visit</header>
+                <section>
+                    <address>2615 Saint Johns Street</address>
+                    <address>Port Moody, BC V3H 2B5, Canada</address>
+                </section>
+            </div>
+            <div class="contact-section">
+                <header>Connect</header>
+                <section>
+                    <p>hello@creativeedgeconsulting.com</p>
+                    <p>778.896.0089</p>
+                </section>
+            </div>
+        </div>
+    </main>
+
+<?php
+    get_footer();
+?>
