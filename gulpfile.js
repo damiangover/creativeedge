d4cb7097c7978c044282e459a096dde341042235
Damian Gover <c3109954@uon.edu.au>
2018-03-08 14:19:09 +1100

    Rework Gulp File
    * Include gulp-sass too

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,23 @@ const gulp = require('gulp');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const newer = require('gulp-newer');
+const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
 const source = {
     root: "./",
     build: "./wp-creativeedge/",
-    mamp: "/MAMP/htdocs/wordpress/wp-content/themes/wp-creativeedge/"
+    mamp: ""
 };
 
-gulp.task('css', function () {
+const port = {
+    nginx: 7888,
+    apache: 8888,
+};
+
+gulp.task('css::prepare', function () {
     var plugins = [
-        autoprefixer({browsers: ['last 4 versions']}),
+        autoprefixer({ browsers: ['last 4 versions'] }),
         cssnano()
     ];
 
@@ -24,27 +30,34 @@ gulp.task('css', function () {
         .pipe(gulp.dest(source.build));
 });
 
-gulp.task('php', function () {
+gulp.task('php::move', function () {
     return gulp.src(source.root + '*.php')
         .pipe(newer(source.build))
         .pipe(gulp.dest(source.build));
 });
 
-gulp.task('mamp', function () {
+gulp.task('mamp::move', function () {
     return gulp.src(source.build + '*.*')
         .pipe(gulp.dest(source.mamp));
 });
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync::start', function () {
     browserSync.init({
-        proxy: "http://localhost/wordpress",
-        browser: ["chrome", "firefox", "opera"]
+        proxy: "http://localhost:" + port.nginx + "/wordpress",
+        browser: ["chrome", "firefox"]
     });
-    gulp.watch(source.mamp + '*.css').on("change", browserSync.reload);
+    gulp.watch(source.build + '*.css').on("change", browserSync.reload);
 });
 
-gulp.task('watch', function() {
-    gulp.watch(source.root + "*.*", ['css', 'php', 'mamp']);
+gulp.task('sass::compile', function () {
+    return gulp.src(source.root + "/scss/*.scss")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(source.root));
 });
 
-gulp.task('default', ['watch']);
+gulp.task('ce::watch', function () {
+    gulp.watch(source.root + "*.*", ['css::prepare', 'php::move']);
+    gulp.watch(source.root + "/scss/*.scss", ['sass::compile']);
+});
+
+gulp.task('default', ['ce::watch']);
