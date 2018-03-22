27a8daf9b8059fc470e51fd206951eab255cb0d4
Damian Gover <c3109954@uon.edu.au>
2018-03-22 14:14:26 +1100

    Add Parent Page Template
    * Targets the conflict/crisis pages with service children

---
+++ page-conflict.php
@@ -0,0 +1,36 @@
+<?php
+/* Template Name: Parent Page Template */
+get_header();
+?>
+
+<main id="post" class="headline animate-text--2s"> 
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <div class="headline-title"><?php the_title(); ?></div>
+        <div class="headline-subtitle"><?php the_content(); ?></div>        
+    <?php endwhile; endif; ?> 
+</main>
+
+<?php
+    $args = array(
+        'parent' => $post->ID,
+        'post_type' => 'page',
+        'post_status' => 'publish',
+    );
+
+    $pages = get_pages($args);
+?>
+
+<div class="card-group">
+    <?php foreach ($pages as $page) { ?>
+        <div class="card">
+            <a href="<?php echo get_permalink($page->ID); ?>" rel="bookmark" title="<?php echo $page->post_title; ?>">
+            <header class="card-header"><h2><?php echo $page->post_title; ?></h2></header>
+            <section class="card-body"><?php $excerpt = get_the_excerpt( $page->ID ); echo $excerpt; ?></section>
+            </a>
+        </div>
+    <?php }?>
+</div>
+
+<?php
+get_footer();
+?>
