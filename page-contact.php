682729b2c45542a9920b76635efb8f2a3234f45b
Damian Gover <c3109954@uon.edu.au>
2018-03-30 00:04:55 +1100

    Finalise Changes for Contact Page with Custom Fields

--- page-contact.php
+++ page-contact.php
@@ -3,33 +3,26 @@
 get_header();
 ?>
 
-    <main class="content animate-text--2s">
-        <div class="contact">
+    <main class="content contact animate-text--2s">
+        <?php if (have_posts()): while (have_posts()): the_post();?>
             <div class="contact-section">
-        
-            <?php if (have_posts()): while (have_posts()): the_post();?>
                 <header>About</header>
                 <section>
                     <?php the_content(); ?>
                 </section>
-                </div>
-                <div class="contact-section">
-                    <header>Visit</header>
-                    <section>
-                        <address>2615 Saint Johns Street</address>
-                        <address>Port Moody, BC V3H 2B5, Canada</address>
-                    </section>
-                </div>
-                <div class="contact-section">
-                    <header>Connect</header>
-                    <section>
-                        <p><?php echo get_post_meta($post->ID, 'about-connect', true); ?></p>
-                        <p><?php echo get_post_meta($post->ID, 'about-call', true); ?></p>
-                    </section>
-                </div>
-            <?php endwhile; ?>
-            <?php endif;?>
-        </div>
+            </div>
+            <div class="contact-section">
+                <header>Visit</header>
+                <address><?php echo get_post_meta($post->ID, 'about-visit', true); ?></address>
+            </div>
+            <div class="contact-section">
+                <header>Connect</header>
+                <section>
+                    <p><?php echo get_post_meta($post->ID, 'about-connect', true); ?></p>
+                    <p><?php echo get_post_meta($post->ID, 'about-call', true); ?></p>
+                </section>
+            </div>
+        <?php endwhile; endif;?>
     </main>
 
 <?php
