0be3f61dc0cdecfbcec0ddbde587b75aa52686e6
Damian Gover <c3109954@uon.edu.au>
2018-04-03 22:12:51 +1000

    Rework the Front Intro

--- page-landing.php
+++ page-landing.php
@@ -4,19 +4,15 @@ get_header();
 ?>
 
 <div id="pre" class="cover">
-    <section class="grid-container--front">
-        <section class="front--logo animate-text--2s">
-            <!-- <p>Creative Edge</p> -->
-        </section>
-        <section class="front--hero">
-            <p class="front--large"><span class="fadeInFromBottom--3s">al</span><span
-                        class="fadeInFromBottom--4s">-che-</span><span
-                        class="fadeInFromBottom--5s">my</span></p>
-            <p class="front--define"><span class="animate-text--3s"><small>noun</small> \ ˈal-kə-mē \</span></p>
-            <p class="front--define"><span class="animate-text--3s">A seemingly magical process of transformation, creation, or combination<span
-                            class="orange">.</span></span></p>
-        </section>
-    </section>
+    <div class=front>
+        <div class="front--large">
+            <span class="fadeInFromBottom--2s">al</span><span class="fadeInFromBottom--3s">-che-</span><span class="fadeInFromBottom--4s">my</span>
+        </div>
+        <div class="front--define">
+            <p><span class="animate-text--3s"><small>noun</small> \ ˈal-kə-mē \</span></p>
+            <p><span class="animate-text--3s">A seemingly magical process of transformation, creation, or combination<span class="accent">.</span></span></p>
+        </div>
+    </div>
 </div>
 
 <main id="post" class="headline animate-text--2s"> 
