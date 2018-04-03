5d2ffcb01aefd1b7c6116a49b175017018f42505
Damian Gover <c3109954@uon.edu.au>
2018-04-04 00:45:16 +1000

    Simplify Post Link Markup
    In this case we shouldn't need to handle the other cases in conditional (famous last words)

--- category.php
+++ category.php
@@ -17,13 +17,7 @@
                     <header class="entry-header post-heading">
                         <?php
 
-                                if (is_single()) {
-                                    the_title('<h3 class="entry-title">', '</h1>');
-                                } elseif (is_front_page() && is_home()) {
-                                    the_title('<h3 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h1>');
-                                } else {
-                                    the_title('<h3 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-                                }
+                            the_title('<h3 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h3>');
 
                         ?>
                     </header>
