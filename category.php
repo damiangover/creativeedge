1c0647d8cfad59a6f0ee19e76a394d66e5bbd778
Damian Gover <c3109954@uon.edu.au>
2018-04-04 00:46:58 +1000

    Add Conditional Styling for Post Ordering
    * Simply make the first post full-width, moving to feature article features in the future. 
    * More Link Markup Simplification

--- category.php
+++ category.php
@@ -5,6 +5,7 @@
 
             <h1 class="center-text"><?php single_cat_title(); ?></h1>
             <?php
+                $is_first_post = true;
                 $active_category = get_category( get_query_var( 'cat' ) );
                 $active_category_id = $active_category->cat_ID;
                 query_posts($query_string .'&posts_per_page=10');
@@ -12,10 +13,20 @@
            
         </header>
 
+        <div class="related-post--container">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-                    <div class="related-post">
-                        <article>
+            <?php if ( $is_first_post ) : ?>
+                <article class="related-post--full-width">
+                    <header class="entry-header post-heading">
+                        <?php
+                            the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+                        ?>
+                    </header>
+                    <div class="excerpt longer-read"><?php the_excerpt(); ?></div>
+                </article>
+                <?php $is_first_post = false; ?>
+            <?php else: ?>
+                <article class="related-post">
                     <header class="entry-header post-heading">
                         <?php
 
@@ -25,7 +36,7 @@
                     </header>
                     <div class="excerpt longer-read"><?php the_excerpt(); ?></div>
                 </article>
-                    </div>
+            <?php endif; ?>
             <?php endwhile; endif; ?>
         </div>
     </main>
