2d6787919eb2a45b542f32c60b56aaa6955a7765
Damian Gover <c3109954@uon.edu.au>
2018-04-04 00:42:37 +1000

    Remove Sub Form

--- category.php
+++ category.php
@@ -5,22 +5,22 @@
             <div class="category-blurb">
                 <section>
                     <h1><?php single_cat_title(); ?></h1>
-                    <?php
-                    $active_category = get_category( get_query_var( 'cat' ) );
-                    $active_category_id = $active_category->cat_ID;
+            <?php
+                $active_category = get_category( get_query_var( 'cat' ) );
+                $active_category_id = $active_category->cat_ID;
                     echo category_description( $active_category_id );
                     query_posts($query_string .'&posts_per_page=3');  ?>
                 </section>
             </div>
-
+           
             <div class="related-post-container">
 
-                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
                     <div class="related-post">
                         <article>
-                            <header class="entry-header post-heading">
-                                <?php
+                    <header class="entry-header post-heading">
+                        <?php
 
                                 if (is_single()) {
                                     the_title('<h3 class="entry-title">', '</h1>');
@@ -30,23 +30,13 @@
                                     the_title('<h3 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
                                 }
 
-                                ?>
-                            </header>
-                            <div class="excerpt longer-read"><?php the_excerpt(); ?></div>
-                        </article>
+                        ?>
+                    </header>
+                    <div class="excerpt longer-read"><?php the_excerpt(); ?></div>
+                </article>
                     </div>
-                <?php endwhile; endif; ?>
-            </div>
-
-            <div class="subscribe">
-                <p>Strategies for change. Straight to your inbox.</p>
-                <form>
-                    <fieldset>
-                        <input type="email" required placeholder="Email Address" class="big-text"/>
-                        <input type="submit" value="Join" class="big-button"/>
-                    </fieldset>
-                </form>
-            </div>
-        </main>
+            <?php endwhile; endif; ?>
+        </div>
+    </main>
     </div>
 <?php get_footer(); ?>
