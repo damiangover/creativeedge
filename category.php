92e60531955598ea9ad90835de6f633c8f8cf50a
Damian Gover <c3109954@uon.edu.au>
2018-04-04 08:56:26 +1000

    Drop Page Title for Styled First Read

--- category.php
+++ category.php
@@ -2,18 +2,11 @@
 
     <main class="browse animate-text--2s">
         <header>
-
-            <h1 class="center-text"><?php single_cat_title(); ?></h1>
+            <!-- <h1 class="center-text"><?php single_cat_title(); ?></h1> -->
             <?php
                 $is_first_post = true;
                 $active_category = get_category( get_query_var( 'cat' ) );
                 $active_category_id = $active_category->cat_ID;
-                $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-                $args = array(
-                    'posts_per_page' => 1,
-                    'paged'          => $paged
-                );
-                $the_query = new WP_Query( $args );
             ?>
         </header>
 
@@ -23,10 +16,11 @@
                     <article class="related-post--full-width">
                         <header class="entry-header post-heading">
                             <?php
-                                the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+                                the_title('<h1 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', ' &horbar;</a></h1>');
                             ?>
                         </header>
-                        <div class="excerpt longer-read"><?php the_excerpt(); ?></div>
+                        <div class="excerpt top-read longer-read"><?php the_excerpt(); ?></div>
+                        <hr />
                     </article>
                 <?php else: ?>
                     <article class="related-post">
